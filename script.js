c8c737574306216f5b00c317d0ec908aa708d19c
Adéla Vajlichová <adell.vajlichova@seznam.cz>
2026-04-21 18:49:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ if (!guestName) {
     guestName = "host";
 }
 
-emailjs.init("vViWQlU51gqIyzRWG");
+emailjs.init("3ZJ8z_cmHx8EN_lPw");
 
 const canvas = document.getElementById("scratch");
 const ctx = canvas.getContext("2d");
