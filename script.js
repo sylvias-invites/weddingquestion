d6e79aabda30d0ae94db30e729af22ab31a01e68
Adéla Vajlichová <adell.vajlichova@seznam.cz>
2026-04-21 19:05:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -323,6 +323,7 @@ startCountdown();
 function answerYes() {
     // 1. Najdeme kontejner s textem
     emailjs.send("service_6omrk9m", "template_ealon11", {
+        guestName: guestName,
 
         message: `${guestName} potvrdila, že bude tvá družička💖`,
 
